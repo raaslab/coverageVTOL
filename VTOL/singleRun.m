1dbb6141b1357969602a44034999439b31835b45
Kevin Yu <klyu@vt.edu>
2020-02-10 11:02:39 -0500

    added exampleBC data

--- VTOL/singleRun.m
+++ VTOL/singleRun.m
@@ -4,16 +4,16 @@
 clc; clear all; close all;
 
 % numBC = 20;
-max_Distance = 20;   % if max_Distance == j then discharge is unit rate per distance (budget)
-j = 50;
+max_Distance = 1800;   % if max_Distance == j then discharge is unit rate per distance (budget)
+j = 20;
 tTO = 5;           % take off cost
 tL = 45;            % landing cost
 rRate = 2;         % rate of recharge
-UGVS = 100;          % time to travel one unit for the UGV to multi-rotor (greater than 1 means UGV is slower)
+UGVS = 5;          % time to travel one unit for the UGV to multi-rotor (greater than 1 means UGV is slower)
 method = 1;        % 1 = GLNS, 0 = concorde
 timeI = [];
-fixedRatio = 6;     % ratio of multi-rotor to fixed-wing battery usage per distance (1lvl/5m : 1lvl/10m = 2) (greater than 1 means fixed wing goes farther on one battery level)
-turnRadius = 0.5;     % turn radius for dubins constraints in 
+fixedRatio = 3;     % ratio of multi-rotor to fixed-wing battery usage per distance (1lvl/5m : 1lvl/10m = 2) (greater than 1 means fixed wing goes farther on one battery level)
+turnRadius = 3;     % turn radius for dubins constraints in 
 
 % changing number of input BC
 
@@ -22,7 +22,7 @@ turnRadius = 0.5;     % turn radius for dubins constraints in
 
 % TODO: check why the path from every node to the new last node is always type 1 edge?
 % use 'testInput.txt' if you want the file from polygonCreater
-data = readData('/home/user01/Kevin_Yu/3D_bridge_meshes/coverage/VTOL/inputs/qualitative.txt'); % get the size and shape from the data (this will tell you number of clusters points and so on)
+data = readData('/home/user01/Kevin_Yu/coverageVTOL/VTOL/inputs/exampleBC/exampleFigureBC.txt'); % get the size and shape from the data (this will tell you number of clusters points and so on)
 [numClusters, ~] = size(data);
 x = [data(:,1), data(:,4)];
 y = [data(:,2), data(:,5)];
@@ -32,10 +32,10 @@ G = 0;
 
 i = numClusters*2; % number of vertices needed to be multiplied by battery levels
 
-filename1 = sprintf('journalQi1');
-filename2 = sprintf('/home/user01/Kevin_Yu/3D_bridge_meshes/coverage/VTOL/outputs/journalQi2.gtsp');
-filename3 = sprintf('journalQi3');
-pathName = '/home/user01/Kevin_Yu/3D_bridge_meshes/coverage/VTOL/outputs';
+filename1 = sprintf('journal1');
+filename2 = sprintf('/home/user01/Kevin_Yu/coverageVTOL/VTOL/outputs/exampleBC/journal2.gtsp');
+filename3 = sprintf('journal3');
+pathName = '/home/user01/Kevin_Yu/coverageVTOL/VTOL/outputs/exampleBC';
 
 tic
 % pathName = '/home/klyu/lab/coverageWork/testForCoverage/errorInstance'; % for error instances
