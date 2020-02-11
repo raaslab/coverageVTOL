5f750e92d0e7d30d0c5278af1976759622bc1e55
Kevin Yu <klyu@vt.edu>
2020-02-11 14:01:35 -0500

    updated exampleBC

--- VTOL/sandbox.m
+++ VTOL/sandbox.m
@@ -1,20 +1,9 @@
-% RSR, RSL, LSR, LSL, RLR, LRL
+fname='exampleBC';
 
-% p = [x, y, theta]
-% r = 4
+saveas(gcf,[fname,'.eps'],'epsc');
 
+system(['epstopdf',' ',[fname,'.eps']]);
 
-p1x = randi([0,100]);
-p1y = randi([0,100]);
-p1theta = 2*pi*rand;
-p2x = randi([0,100]);
-p2y = randi([0,100]);
-p2theta = 2*pi*rand;
-p1 = [p1x,p1y,p1theta];
-p2 = [p2x,p2y,p2theta];
-r = randi([1,10]);
-stepsize = -1;
-quiet = false;
+system(['pdfcrop',' ',[fname,'.pdf']]);
 
-param = dubins_core(p1, p2, r);
-path = dubins_curve(p1, p2, r, stepsize, quiet);
+system(['mv',' ',[fname,'-crop.pdf'],' ',[fname,'.pdf']]);
